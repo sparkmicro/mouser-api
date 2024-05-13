e2e195a7ddef9491380335fe2f8ec910c19b7472
noname <nospam@dontspamme.com>
2024-05-13 16:56:58 -0400

    Add Mouser Keyword Search

--- mouser/cli.py
+++ mouser/cli.py
@@ -15,8 +15,10 @@ API_KEYS_FILE = 'mouser_api_keys.yaml'
 @click.argument('request_type', type=click.Choice(['cart', 'history', 'order', 'search'], case_sensitive=False), required=True)
 @click.argument('operation', required=True)
 @click.option('--number', required=False, help='Part or Order number.')
+@click.option('--keyword', required=False, help='Keyword(s) to search')
+@click.option('--record-limit' , required=False, help='How many records --keyword search should return')
 @click.option('--export', is_flag=True, required=False, help='Export data to CSV.')
-def mouser_cli(request_type, operation, number, export):
+def mouser_cli(request_type, operation, number, keyword, record_limit, export):
     ''' Main CLI entry point '''
 
     args = []
@@ -59,6 +61,21 @@ def mouser_cli(request_type, operation, number, export):
                         # Print result
                         print('[DATA]')
                         request.print_clean_response()
+            
+            if operation == 'keyword':
+                if not keyword:
+                    print('[ERROR]\t Missing --keyword flag and keyword(s)')
+                else:
+                    # Run request
+                    search = request.keyword_search(keyword, record_limit)
+                    # Print body
+                    print('[BODY]')
+                    print(json.dumps(request.body, indent=4, sort_keys=True))
+                    if search:
+                        # Print result
+                        print('[DATA]')
+                        request.print_clean_response()
+
 
     elif request_type == 'cart':
         request = MouserCartRequest(operation, API_KEYS_FILE, *args)
